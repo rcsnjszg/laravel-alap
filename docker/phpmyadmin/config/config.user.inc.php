73afda7d02aa2ce789f0def2cb973e9b71ae8d9d
rcsnjszg <49613681+rcsnjszg@users.noreply.github.com>
2022-10-19 14:58:38 +0200

    docker init

---
+++ docker/phpmyadmin/config/config.user.inc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * phpMyAdmin sample configuration, you can use it as base for
+ * manual configuration. For easier setup you can use setup/
+ *
+ * All directives are explained in documentation in the doc/ folder
+ * or at <https://docs.phpmyadmin.net/>.
+ */
+
+declare(strict_types=1);
+
+
+$cfg['fontsize'] = '200%';
