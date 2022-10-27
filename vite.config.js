36df9a9fea9efbdacf6aa62e237490605fa8d1d3
rcsnjszg <49613681+rcsnjszg@users.noreply.github.com>
2022-10-27 14:47:46 +0200

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+export default ({ mode }) => {
+    process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+
+    return defineConfig({
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+        ],
+        server: {
+            port:  process.env.VITE_PORT
+        }
+    });
+}
