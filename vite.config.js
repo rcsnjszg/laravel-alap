772a039b1cddb5457efb7e2d29b3c6381872559e
rcsnjszg <49613681+rcsnjszg@users.noreply.github.com>
2022-10-27 20:42:32 +0200

    css replaced by scss

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default ({ mode }) => {
     return defineConfig({
         plugins: [
             laravel({
-                input: ['resources/css/app.css', 'resources/js/app.js'],
+                input: ['resources/css/app.scss', 'resources/js/app.js'],
                 refresh: true,
             }),
         ],
